fbf3b76169e71c54e9aa2f56df1c6e26bbe23bf7
deblynprado <deblynprado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-06 04:44:32 +0000

    Updating to version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-outdated-browser/tags/2.0.0@943821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
-//PLAIN JAVASCRIPT
-//event listener form DOM ready
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function() {
-            oldonload();
-            func();
-        }
-    }
-}
-//call plugin function after DOM ready
-addLoadEvent(
-    outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform'
-    })
-    );
-
-//USING jQuery
-$( document ).ready(function() {
-    outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform'
-    })
+outdatedBrowser({
+   bgColor: outOptions.bkgColor,
+   color: outOptions.fontColor,
+   lowerThan: outOptions.browser
 })
+
+console.log(outdatedBrowser);
